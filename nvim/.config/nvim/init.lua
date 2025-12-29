e26b22e0406c60bddda4da044902010f7d87a4b0
PiotrKopczynski <piotrkopczynski52@gmail.com>
2025-12-30 00:02:18 +0100

    Making the mini.files menu open up the directory of the currently opened buffer

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1229,8 +1229,8 @@ require('lazy').setup({
       }
 
       vim.keymap.set('n', '-', function()
-        require('mini.files').open()
-      end, { desc = 'Open mini.files' })
+        require('mini.files').open(vim.api.nvim_buf_get_name(0))
+      end, { desc = 'Open mini.files (current file)' })
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
